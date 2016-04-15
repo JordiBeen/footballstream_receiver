1c344e3023db113563c70a87bf2ef354fd2b0649
jordibeen <jordibeen1996@gmail.com>
2016-04-15 13:25:06 +0200

    Filling data for matches

--- js/divide.js
+++ js/divide.js
@@ -42,7 +42,15 @@ function splitScreen() {
     // Create HTML according to match amount
     var html = "";
     for (var i = 0; i < matchAmount; i++) {
-        html += '<div data-match-id=' + matchArray[i] + ' class="match col-md-' + columnClass + '"><h1>MATCH</h1></div>';
+        html += '<div data-match-id=' + matchArray[i] + ' class="match col-md-' + columnClass + '">\
+                    <h1 class="matchup"></h1>\
+                    <h2 class="score"></h2>\
+                    <p class="stadium"></p>\
+                    <p class="attendance"></p>\
+                    <p class="referee"></p>\
+                    <div class="events"></div>\
+                    <div class="competition"></div>\
+                </div>';
     };
 
     // Append the HTML to the container
@@ -53,21 +61,49 @@ function splitScreen() {
 }
 
 function getDataForEachMatch() {
-    var matchInfo = {}
     var $matches = $(".match");
     var matchesLength = $matches.length;
+
+    // Object to be filled with matchid:matchobject structure
+    var matchesObject = {}
+
+    // Array to store get requests to be made
+    var gets = [];
+
+    // Loop through all matches and create get request according to match id
     $matches.each(function(i) {
         $this = $(this);
         var matchId = $this.data('match-id');
-        $.get("http://footballstream-api.jordibeen.nl/api/v1/matches/" + matchId, function(data) {
-            matchInfo[matchId] = data;
-            if (i == matchesLength - 1) {
-                fillDataForEachMatch(matchInfo);
+        gets.push($.ajax({
+            type: "GET",
+            url: "http://footballstream-api.jordibeen.nl/api/v1/matches/" + matchId,
+            success: function(data) {
+                matchesObject[matchId] = data;
             }
-        });
+        }));
     });
+
+    // Fire off all get requests, and fill the data when they are done
+    $.when.apply($, gets).then(function() {
+        fillDataForEachMatch(matchesObject);
+    })
 }
 
-function fillDataForEachMatch(matchInfo){
-    console.log(matchInfo);
+function fillDataForEachMatch(matchesObject) {
+    // Loop through all matches in the matches object
+    for (matchId in matchesObject) {
+        // Get match info object per match
+        var matchInfo = matchesObject[matchId]['match'];
+        // Look for base html for this match 
+        // will return empty HTML node already loaded in the DOM
+        var $matchNode = $("div[data-match-id='" + matchId + "'");
+
+        // MATCHUP
+        $matchNode.find('.matchup').html(matchInfo['matchup']);
+        // SCORE
+        $matchNode.find('.score').html(matchInfo['visitorteam_score'] + " - " + matchInfo['localteam_score']);
+        // STADIUM
+        $matchNode.find('.stadium').html(matchInfo['stadium']);
+
+    }
 }
